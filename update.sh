fdfc34673540df1a474efef2e2418cb96c1ff875
mbuhl <git@moritzbuhl.de>
2025-01-10 11:23:20 +0100

    Add script to check for updates.

---
+++ update.sh
@@ -0,0 +1,32 @@
+#!/bin/sh
+
+set -eu
+
+TEMP=$(mktemp)
+
+function cleanup {
+  rm "$TEMP"
+}
+trap cleanup EXIT
+
+ftp -o $TEMP https://freedict.org/freedict-database.json
+
+for ne in $(jq '.[] | [.name, .edition] | @sh' $TEMP | tr -d ' '); do
+	name="${ne#\"\'}"
+	name="${name%\'\'*}"
+	if [ "$name" = '"nullnull"' ]; then
+		break;
+	fi
+	edition="${ne##\"\'*([!\'])\'\'}"
+	edition="${edition%\'\"}"
+	if [ -d /usr/ports/education/freedict/$name ]; then
+		port="$(make -C /usr/ports/education/freedict/$name -V V)"
+		if [ "$port" != "$edition" ]; then
+			#echo "$name: $port vs $edition"
+			sed -i "s/$port/$edition/g" \
+			    /usr/ports/education/freedict/$name/Makefile
+		fi
+	else
+	    echo "$name: missing"
+	fi
+done
