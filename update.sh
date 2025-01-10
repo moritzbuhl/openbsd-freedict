79be15c27e2e67d1bb6b6e43a638259568e98d54
mbuhl <git@moritzbuhl.de>
2025-01-10 11:26:32 +0100

    Apply shellcheck any formatting.

--- update.sh
+++ update.sh
@@ -4,14 +4,14 @@ set -eu
 
 TEMP=$(mktemp)
 
-function cleanup {
-  rm "$TEMP"
+cleanup() {
+	rm "$TEMP"
 }
 trap cleanup EXIT
 
-ftp -o $TEMP https://freedict.org/freedict-database.json
+ftp -o "$TEMP" https://freedict.org/freedict-database.json
 
-for ne in $(jq '.[] | [.name, .edition] | @sh' $TEMP | tr -d ' '); do
+for ne in $(jq '.[] | [.name, .edition] | @sh' "$TEMP" | tr -d ' '); do
 	name="${ne#\"\'}"
 	name="${name%\'\'*}"
 	if [ "$name" = '"nullnull"' ]; then
@@ -19,12 +19,12 @@ for ne in $(jq '.[] | [.name, .edition] | @sh' $TEMP | tr -d ' '); do
 	fi
 	edition="${ne##\"\'*([!\'])\'\'}"
 	edition="${edition%\'\"}"
-	if [ -d /usr/ports/education/freedict/$name ]; then
-		port="$(make -C /usr/ports/education/freedict/$name -V V)"
+	if [ -d "/usr/ports/education/freedict/$name" ]; then
+		port="$(make -C "/usr/ports/education/freedict/$name" -V V)"
 		if [ "$port" != "$edition" ]; then
 			#echo "$name: $port vs $edition"
 			sed -i "s/$port/$edition/g" \
-			    /usr/ports/education/freedict/$name/Makefile
+			    "/usr/ports/education/freedict/$name/Makefile"
 		fi
 	else
 	    echo "$name: missing"
