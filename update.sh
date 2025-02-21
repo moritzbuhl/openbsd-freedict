abe9f08fd3e985c7ae645d9ef6a9c1180d46d6f3
mbuhl <git@moritzbuhl.de>
2025-02-21 10:37:58 +0100

    Add the status for missing ports.

--- update.sh
+++ update.sh
@@ -11,22 +11,24 @@ trap cleanup EXIT
 
 ftp -o "$TEMP" https://freedict.org/freedict-database.json
 
-for ne in $(jq '.[] | [.name, .edition] | @sh' "$TEMP" | tr -d ' '); do
-	name="${ne#\"\'}"
-	name="${name%\'\'*}"
-	if [ "$name" = '"nullnull"' ]; then
+for l in $(jq '.[] | [.name, .edition, .status] | @sh' "$TEMP" | tr ' ' _); do
+	name="${l#\"\'}"
+	name="${name%%\'_*}"
+	if [ "$name" = '"null_null_null"' ]; then
 		break;
 	fi
-	edition="${ne##\"\'*([!\'])\'\'}"
-	edition="${edition%\'\"}"
+	edition="${l##\"\'*([!\'])\'_\'}"
+	edition="${edition%%\'_*}"
+	reason="${l##*_\'}"
+	reason="${reason%\'\"}"
 	if [ -d "/usr/ports/education/freedict/$name" ]; then
 		port="$(make -C "/usr/ports/education/freedict/$name" -V V)"
 		if [ "$port" != "$edition" ]; then
-			#echo "$name: $port vs $edition"
+			echo "$name: $port vs $edition"
 			sed -i "s/$port/$edition/g" \
 			    "/usr/ports/education/freedict/$name/Makefile"
 		fi
 	else
-	    echo "$name: missing"
+	    echo "$name: missing, $(echo "$reason" | tr _ ' ')"
 	fi
 done
